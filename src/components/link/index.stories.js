7888f664b8fe3d64635a8f220fbb4f9fa49370c6
Artur Krol <kroolar@gmail.com>
2024-02-12 08:59:32 +0100

    Improve link component

--- src/components/link/index.stories.js
+++ src/components/link/index.stories.js
@@ -4,15 +4,30 @@ export default {
   title: "Link",
   component: Component,
   argTypes: {
+    brightness: {
+      options: ["xs", "sm", "md", "lg", "xl"],
+      control: { type: "inline-radio" }
+    },
     size: {
       options: ["xs", "sm", "md", "lg", "xl"],
       control: { type: "inline-radio" }
-    }
+    },
+    weight: {
+      options: ["xs", "sm", "md", "lg", "xl"],
+      control: { type: "inline-radio" }
+    },
   }
 }
 
 export const Primary = {
   args: {
+    brightness: "md",
+    className: "",
+    href: "/",
+    render: true,
+    size: "md",
     text: "Link to",
+    weight: "md"
   },
 }
+
--- src/components/link/index.tsx
+++ src/components/link/index.tsx
@@ -2,26 +2,36 @@ import React from "react"
 import { Size } from "../../types"
 
 type Props = {
+  brightness?: Size
   className?: string
   href: string
   render?: boolean
   size?: Size
   text: string
+  weight?: Size
 }
 
 const Link = ({
+  brightness = "md",
   className = "",
   href,
   render = true,
   size = "md",
-  text
+  text,
+  weight = "md"
 }: Props) => {
   if (!render) return null
 
   return (
     <a
       href={href}
-      className={`link link--${size} ${className}`}
+      className={`
+        link
+        link--${size}
+        brightness-${brightness}
+        weight-${weight}
+        ${className}
+      `}
     >
       {text}
     </a>
